6c5260b1841ba2960945193fde9a4d379802814f
Valerio Sevilla <valerio.sevilla@gmail.com>
2017-02-26 10:00:20 +0100

    REMOVE Useless code from the server

--- src/server.js
+++ src/server.js
@@ -9,24 +9,6 @@ module.exports = (() => {
 
 		constructor() {
 			this._server = express();
-			// eslint-disable-next-line
-			this._router = express.Router();
-		}
-
-		applyRoutes() {
-			var routerInstance = new Router();
-			routerInstance.apply(this._router);
-		}
-
-		handleErrors() {
-			this._server.use(function (error, request, response, next) {
-				if (error instanceof SyntaxError) {
-					response.status(400).send(syntaxErrorMessage);
-				} else {
-					console.log(error);
-					response.status(500).send(unknownErrorMessage);
-				}
-			});
 		}
 
 		init() {
